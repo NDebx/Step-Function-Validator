8a7af5256093143cdc78672935f97f7625fb0c8a
NileshDebix <1025893@hr.nl>
2022-02-26 23:41:31 +0100

    cli tool completion but yaml control must be little bit less strict

--- app.py
+++
@@ -1,8 +0,0 @@
-from jsonschema import validate
-import yaml
-
-yaml_schema = open("test.yaml", "r")
-json_schema = open("test.json", "r")
-
-
-validate(yaml.full_load(json_schema.read()), yaml.full_load(yaml_schema.read())) # passes
